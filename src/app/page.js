cf25f72d257cae33fc1afe177d3b0b5e1eceaead
DanielDiazRuiz2002 <102238701+DanielDiazRuiz2002@users.noreply.github.com>
2023-10-20 05:02:24 -0500

    Mejora en el HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Product from "@/components/common/Product/Product";
-import { Button } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <div className="bg-gray-300 h-3/5 w-full mb-12 pl-32 py-24">
+      <div className="h-3/5 w-full mb-12 pl-32 py-24 bg-cover bg-center" style={{backgroundImage: `url('/images/FondoHome.png')`}}>
         <p className="text-white font-bold text-6xl drop-shadow-xl mb-3">
           ¿No te gusta lo que comes?
         </p>
@@ -20,7 +20,7 @@ export default function Home() {
           Reserva tu menú ahora
         </Button>
       </div>
-      <div className="flex w-4/5 h-auto mx-auto space-x-2 mb-10">
+      <div className="flex justify-center w-4/5 h-auto mx-auto space-x-2 mb-10">
         <Product />
         <Product />
         <Product />
